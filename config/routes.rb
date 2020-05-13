46d1c4e57a9a9c29c4f058fedb45df84424b06a1
Watanabe Hiroki <hiroki@WatanabenoMacBook-puro.local>
2020-05-13 23:20:48 +0900

    db:migrate && g Pages index home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/home'
   devise_for :users
   resources :posts
-  root to: "home#index"
+  root to: "pages#index"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
