71a2cf36a6f35b4ae097ea5bc80d6ba060ad3aa6
Watanabe Hiroki <hiroki@WatanabenoMacBook-puro.local>
2020-05-23 16:41:12 +0900

    edit_pages#index && ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    #user ||= User.new
+    can :read,  :all
+
+    if user.admin? 
+      can :manage,  :all
+
+    end
+
+
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
